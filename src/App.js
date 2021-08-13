bea720217cb3421a1658a7a7aaac5218baf29b20
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-08-13 22:55:23 +0500

    добавила класс

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Container from 'components/Container/Container';
 import ContactForm from 'components/ContactForm/ContactForm';
-import { v4 as uuidv4 } from 'uuid';
+
 
 
 
@@ -11,15 +11,7 @@ class App extends Component {
     name: ''
   }
 
-  nameInputId = uuidv4(); 
-
-  handleChange = e => {
-    console.log(e.currentTarget);
-    const { name, value } = e.currentTarget;
-
-    this.setState({ [name]: value });
-  };
-
+  
   render() {
     // const {name} = this.state;    
 
@@ -27,8 +19,7 @@ class App extends Component {
       <Container>
       <div>
       <h1>Phonebook</h1>
-      <ContactForm
-      />
+      <ContactForm/>
      
       <h2>Contacts</h2>
       {/* <Filter ... />
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,23 +1,41 @@
 import { Component } from 'react';
 import s from './ContactForm.module.css';
+import { v4 as uuidv4 } from 'uuid';
 
- class ContactForm extends Component {
+class ContactForm extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
 
-    render() {
-        return (
-            <form className={s.form}>
-              <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required  
-              />
-              <button type="submit">Add contact</button>
-            </form>
-          );
-    }
-  
+  nameInputId = uuidv4();
+
+  handleChange = e => {
+    console.log(e);
+    const { name, value } = e.currentTarget;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { name } = this.state;
+
+    return (
+      <form className={s.form}>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+          value={name}
+          onCange={this.handleChange}
+          id={this.nameInputId}
+        />
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
 }
 
 export default ContactForm;

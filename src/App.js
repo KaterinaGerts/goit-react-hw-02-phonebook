cc182cf7b1f73d024fcf8e819199d9b5dd3fee92
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-08-15 16:10:22 +0500

    сделала добавление и удаление контактов

--- src/App.js
+++ src/App.js
@@ -1,32 +1,60 @@
 import React, { Component } from 'react';
 import Container from 'components/Container/Container';
 import ContactForm from 'components/ContactForm/ContactForm';
-
-
-
+import ContactList from 'components/ContactList/ContactList';
+// import Filter from 'components/Filter/Filter';
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends Component {
   state = {
-    contacts: [],    
-  }
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
 
   formSubmitHandler = data => {
     console.log(data);
-  }
-  
+  };
+
+addContact = ({name, number}) => {
+    const data = {
+      id: uuidv4(),
+      name,
+      number,
+    };
+
+    this.setState(({contacts}) => ({
+      contacts: [...contacts, data],
+    }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
   render() {
-    
     return (
       <Container>
-      <div>
-      <h1>Phonebook</h1>
-      <ContactForm onSubmit={this.formSubmitHandler}/>
-     
-      <h2>Contacts</h2>
-      {/* <Filter ... />
-      <ContactList ... /> */}
-    </div>
-    </Container>
+        <div>
+          <h1>Phonebook</h1>
+          <ContactForm 
+          onSubmit={this.addContact}           
+          />
+
+          <h2>Contacts</h2>
+          {/* <Filter />  */}
+          <ContactList          
+            contacts={this.state.contacts}
+            onDeleteContact={this.deleteContact}
+          />
+        </div>
+      </Container>
     );
   }
 }
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import s from './ContactForm.module.css';
 import PropTypes from 'prop-types';
-import { v4 as uuidv4 } from 'uuid';
 
 class ContactForm extends Component {
   state = {
@@ -17,16 +16,15 @@ class ContactForm extends Component {
   handleSubmit =e => {
     e.preventDefault();
 
-    this.props.onSubmit(this.state);
+    const {name, number} = this.state
+
+    this.props.onSubmit({name, number});    
     this.reset();
   };
 
   reset = () => {
     this.setState({name: '', number: '',});
   };
-
-  inputNameId = uuidv4();
-  inputNunberId = uuidv4();
 
   render() {
     const { name, number } = this.state;
@@ -40,8 +38,7 @@ class ContactForm extends Component {
             type="text"
             name="name"
             value={name}
-            onChange={this.handleChange}
-            id={this.inputNameId}
+            onChange={this.handleChange}           
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
             required
@@ -54,8 +51,7 @@ class ContactForm extends Component {
             type="tel"
             name="number"
             value={number}
-            onChange={this.handleChange}
-            id={this.inputNunberId}
+            onChange={this.handleChange}           
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
             required
@@ -70,8 +66,8 @@ class ContactForm extends Component {
 }
 
 ContactForm.propTypes = {
-  name: PropTypes.string.isRequired,
-  number: PropTypes.string.isRequired,
+  name: PropTypes.string,
+  number: PropTypes.number,
 };
 
 export default ContactForm;
